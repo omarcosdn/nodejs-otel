422d6009d20ff71f3901a6da213e40994804cee4
Marcos Demetrio <demetrius.marcos@gmail.com>
2024-12-01 16:28:35 -0300

    add post-api

---
+++ post-api/src/http-client.ts
@@ -0,0 +1,33 @@
+import axios, { AxiosInstance } from "axios";
+
+const getHttpClient = (): AxiosInstance => {
+    return axios.create({
+        baseURL: 'http://localhost:3001',
+        timeout: 30,
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+}
+
+export type Post = {
+    id: number;
+    title: string;
+    body: string;
+    likes_count: number;
+};
+
+export type PostResponse = {
+    content: Post[];
+};
+
+const getPosts = async (): Promise<Post[]> => {
+    const { data } = await getHttpClient().get<PostResponse>("/post-service/post");
+    return data.content ?? [];
+};
+
+const incrementLike = async (postId: number): Promise<void> => {
+    await getHttpClient().post<Post[]>(`/post-service/post/like/${ postId }`);
+};
+
+export { getPosts, incrementLike };
